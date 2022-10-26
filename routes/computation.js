6f9f8298b16617ad577569ad1727d821ed1465b4
ashrithaponugoti <112123590+ashrithaponugoti@users.noreply.github.com>
2022-10-26 18:12:14 -0500

    Changes are done

--- routes/computation.js
+++ routes/computation.js
@@ -1,13 +1,34 @@
 var express = require('express');
 var router = express.Router();
-let x=Math.random();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send(`Math.pow() applied to ${x} is ${Math.pow(x)} <br>
-  Math.sign() applied to ${x} is ${Math.sign(x)} <br>
-  Math.tanh() applied to ${x} is ${Math.tanh(x)} <br>
-  Math.trunc() applied to ${x} is ${Math.trunc(x)}`);
+router.get('/', (req, res, next) => {
+  console.log(req.query)
+  if (Object.keys(req.query).length === 0) {
+
+    console.log("Entered")
+
+    let r = Math.random()
+
+    res.render(`computation`, { value1: `Math.pow() applied to  ${r},${r} is ${Math.pow(r,r)}`,
+     value2: `Math.sign() applied to  ${r} is ${Math.sign(r)}`,
+    value3: `Math.tanh() applied to  ${r} is ${Math.tanh(r)}`, 
+    value4: `Math.trunc() applied to  ${r} is ${Math.trunc(r)}`});
+
+} else
+
+    for (let i in req.query) {
+
+        console.log(i)
+
+        res.render(`computation`, { value1: `Math.pow() applied to  ${req.query[i]},${req.query[i]} is ${Math.pow(req.query[i],req.query[i])}`,
+     value2: `Math.sign() applied to  ${req.query[i]} is ${Math.sign(req.query[i])}`,
+    value3: `Math.tanh() applied to  ${req.query[i]} is ${Math.tanh(req.query[i])}`, 
+    value4: `Math.trunc() applied to  ${req.query[i]} is ${Math.trunc(req.query[i])}`});
+
+    }
+
 });
+
 
 module.exports = router;
