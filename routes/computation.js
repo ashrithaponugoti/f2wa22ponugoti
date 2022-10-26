6e4f9a8ef266eaeba9e942d3c3fc32303c8dad0e
ashrithaponugoti <112123590+ashrithaponugoti@users.noreply.github.com>
2022-10-25 21:21:34 -0500

    edit done

--- routes/computation.js
+++ routes/computation.js
@@ -4,10 +4,10 @@ let x=Math.random();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send(`Math.abs() applied to ${x} is ${Math.abs(x)} <br>
-  Math.acos() applied to ${x} is ${Math.acos(x)} <br>
-  Math.sin() applied to ${x} is ${Math.sin(x)} <br>
-  Math.sinh() applied to ${x} is ${Math.sinh(x)}`);
+  res.send(`Math.pow() applied to ${x} is ${Math.pow(x)} <br>
+  Math.sign() applied to ${x} is ${Math.sign(x)} <br>
+  Math.tanh() applied to ${x} is ${Math.tanh(x)} <br>
+  Math.trunc() applied to ${x} is ${Math.trunc(x)}`);
 });
 
 module.exports = router;
